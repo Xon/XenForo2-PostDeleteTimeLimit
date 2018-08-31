463c9dd750f4383bdec639bd0bb68f4a74615a30
Xon <tazipper1@hotmail.com>
2018-08-31 23:31:30 +1000

    Refactor cleanup

--- upload/src/addons/SV/PostDeleteTimeLimit/XF/Entity/Post.php
+++ upload/src/addons/SV/PostDeleteTimeLimit/XF/Entity/Post.php
@@ -4,26 +4,26 @@ namespace SV\PostDeleteTimeLimit\XF\Entity;
 
 class Post extends XFCP_Post
 {
-	public function canDelete($type = 'soft', &$error = null)
-	{
-		$canDelete = parent::canDelete($type, $error);
+    public function canDelete($type = 'soft', &$error = null)
+    {
+        $canDelete = parent::canDelete($type, $error);
 
-		$visitor = \XF::visitor();
+        $visitor = \XF::visitor();
 
-		$nodeId = $this->Thread->node_id;
+        $nodeId = $this->Thread->node_id;
 
-		if (!$canDelete && !$visitor->hasNodePermission($nodeId, 'deleteAnyPost'))
-		{
-			$deleteLimit = $visitor->hasNodePermission($nodeId, 'deleteOwnPostTimeLimit');
+        if (!$canDelete && !$visitor->hasNodePermission($nodeId, 'deleteAnyPost'))
+        {
+            $deleteLimit = $visitor->hasNodePermission($nodeId, 'deleteOwnPostTimeLimit');
 
-			if ($deleteLimit != -1 && (!$deleteLimit || $this->post_date < \XF::$time - 60 * $deleteLimit))
-			{
-				$error = \XF::phraseDeferred('message_edit_time_limit_expired', ['minutes' => $deleteLimit]);
+            if ($deleteLimit != -1 && (!$deleteLimit || $this->post_date < \XF::$time - 60 * $deleteLimit))
+            {
+                $error = \XF::phraseDeferred('message_edit_time_limit_expired', ['minutes' => $deleteLimit]);
 
-				return false;
-			}
-		}
+                return false;
+            }
+        }
 
-		return $canDelete;
-	}
+        return $canDelete;
+    }
 }
